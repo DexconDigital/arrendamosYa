d1703480d0da57176b82f63fb48af89fb29a3f1e
Manuel <alexthunder7@hotmail.com>
2019-10-10 09:19:12 -0500

    se agregaron textos para quienes somos

--- variables/variables.php
+++ variables/variables.php
@@ -74,30 +74,40 @@ $datos_contacto = [
 
 //logos aliados
 $logos_aliados = [
-	0 => "images/LOGO EL LIBERTADOR.png",
+	0 => "images/LOGO BOLIVAR.png",
 	1 => "images/LOGO SURA.png",
-	2 => "images/LOGO SIMI.png",
+	2 => "images/LOGO FIANZACREDITO.png",
+	3 => "images/LOGO SIMI.png",
 ];
 
 
 //formularios aseguradoras
 $formularios_aseguradoras = [
 	0 => [
-		'nombre' => 'Libertador Persona Jurídica',
+		'nombre' => 'Seguros Bolivar Persona Jurídica',
 		'ruta' => '/'
 	],
 	1 => [
-		'nombre' => 'Libertador Persona Natural',
+		'nombre' => 'Seguros Bolivar Persona Natural',
 		'ruta' => '/'
 	],
 	2 => [
-		'nombre' => 'Sura Persona Natural',
+		'nombre' => 'Sura Persona Jurídica',
 		'ruta' => '/'
 	],
 	3 => [
 		'nombre' => 'Sura Persona Natural',
 		'ruta' => '/'
 	],
+	4 => [
+		'nombre' => 'Fianzacredito Persona Natural',
+		'ruta' => '/'
+	],
+	5 => [
+		'nombre' => 'Fianzacredito Persona Natural',
+		'ruta' => '/'
+	],
+	
 ];
 
 //imagenes slide home
@@ -119,16 +129,42 @@ $imagenes_slide_principal = [
 //textos quienes somos
 $texto_quienes_somos = [
 	"quienes_somos" => [
-		"titulo" => "Quiénes Somos",
 		"parrafos" => [
-			0 => "Somos un equipo de trabajo preparado con alta calidad para proporcionarle a todos nuestros clientes la confianza y tranquilidad que sus procesos de administración y arrendamiento gozan de seguridad legal, con estudios jurídicos apropiados y debidamente afianzados por aseguradoras sólidas en el mercado inmobiliario.",
-			1 => "Realizamos un acompañamiento personalizado de principio a fin, con un asesor personalizado.",
+			0 => "Somos un equipo de trabajo preparado con alta calidad para proporcionarles a todos nuestros clientes la confianza y tranquilidad que sus procesos de administración y arrendamiento requieren. Ofrecemos seguridad legal, con estudios jurídicos apropiados y debidamente afianzados por aseguradoras sólidas en el mercado inmobiliario.",
+			1 => "Realizamos un acompañamiento idóneo de principio a fin, con un asesor personalizado.",
 			2 => "En nuestros procesos realizamos trámites administrativos y jurídicos ante eventualidades que lo ameriten.",
-			3 => " Pertenecemos al Gremio Inmobiliario de la Cámara Nacional Inmobiliaria.",
-			4 => " Estamos afianzados por las aseguradoras El Libertador y Sura.",
+			3 => "Pertenecemos al Gremio Inmobiliario de la Cámara Nacional Inmobiliaria.",
+			4 => "Estamos afianzados por las aseguradoras Sura, Bolívar y Fianzacrédito.",
 		],	
 		"imagen" => "images/",
 	],
+	"quienes_somos_2" => [
+		"titulo" => "ARRENDAMOS YA PROPIEDAD RAÍZ",
+		"parrafos" => [
+			0 => "Somos una empresa del sector inmobiliario, que presta servicios de Arrendamientos, Ventas, Hipotecas, Avalúos y Asesorías Jurídicas en temas relacionados con la actividad inmobiliaria. Contamos con un Equipo Humano de excelentes calidades y riqueza moral y nuestro lema es servir con calidad.",
+			1 => "Nuestra sede principal se encuentra en el Sector Exclusivo de la Alpujarra y tenemos presencia en los 10 Municipios del Área Metropolitana.",
+		],
+	];
+	"mision" => [
+		"titulo" => "Misión",
+		0 => "ARRENDAMOS YA es una empresa del sector inmobiliario que nació para ofrecer a nuestros clientes la confianza, seguridad y tranquilidad de Administrar, Arrendar o Vender propiedades a su entera satisfacción, con procesos modernos y seguros y personal altamente calificado.",
+	];
+	"vision" => [
+		"titulo" => "Visión",
+		0 => "ARRENDAMOS YA, para el año 2.030 seremos una empresa líder y competitiva en la prestación del servicio y mercado inmobiliario del área metropolitana, con procesos certificados en Calidad que beneficien a propietarios, arrendatarios y compradores.",
+	];
+	"objetivos" => [
+		"titulo" => "Objetivos de Calidad",
+		0 => "ARRENDAMOS YA, se propone a corto plazo ofrecer el mejor servicio del mercado y a largo plazo ser una de las empresas mejor posicionadas en el sector inmobiliario.",
+	];
+	"valores" => [
+		"titulo" => "Valores",
+		0 => "Para ARRENDAMOS YA, el Cliente es nuestra razón de ser.",
+		1 => "El Recurso Humano es nuestra mayor riqueza y fortaleza, ya que su compromiso, voluntad y desempeño conforman un valioso activo que en equipo es fuente segura de buen trabajo.",
+		2 => "Las calidades, cualidades e idoneidad de todo el personal están dispuestos a la satisfacción del cliente.",
+		3 => "Estamos siempre atentos a escuchar las necesidades y recomendaciones del cliente que nos retroalimentan y permiten mejorar.",
+		4 => "Siempre habrá alguien que lo escuche y acompañe hasta solucionar sus necesidades e inquietudes.",
+	];
 ];
 
 //textos y banner contador de estadisticas seccion home
